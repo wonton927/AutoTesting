f369dd9afb46cfd9ec40a34e229a34e639a054fe
Ling Qing Kong <wonton601@Lings-iMac.local>
2012-10-22 11:36:36 -0400

    add WowUtilsTest

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,14 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Ling Qing Kong on 10/22/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface WowUtilsTests : SenTestCase
+
+@end
