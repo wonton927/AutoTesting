2aaf8d2e09bebd4a54ea23885e38bd6d964e0c8b
Ling Qing Kong <wonton601@Lings-iMac.local>
2012-10-22 14:57:37 -0400

    add two test cases

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Ling Qing Kong on 10/22/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
